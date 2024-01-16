f193b594814a755fdef27cb91e6f4b3cd4c462c6
Kyle Stewart <kstew1017@outlook.com>
2024-01-16 16:34:42 -0600

    third deployment, moved use clien directive into function scopes, idk

---
+++ app/(root)/homeClient.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useState } from "react";
+import { HeroSection } from "@/components/(Home)/hero/HeroSection";
+import { MeetUsSection } from "@/components/(Home)/meetUs/MeetUsSection";
+import { AnimatedDivider } from "@/components/layout/AnimatedDivder";
+import { TestimonialsSection } from "@/components/(Home)/testimonials/TestimonialsSection";
+import { LocationSection } from "@/components/(Home)/location/LocationSection";
+import { BackgroundIcons } from "@/components/layout/BackgroundIcons";
+import { faScissors, faSpa, faSprayCanSparkles } from "@fortawesome/free-solid-svg-icons";
+
+interface stylistsProps {
+    _id: string;
+    name: string;
+    quote: string;
+    bio: string;
+    paymentMethods: string[];
+    services: string[];
+    icons: string[];
+}
+
+export default function HomeClient({ initialStylists }: { initialStylists: stylistsProps[] }) {
+    const [stylists, setStylists] = useState<stylistsProps[]>(initialStylists);
+
+    return (
+        <>
+            <BackgroundIcons
+                rows={8}
+                icon1={faSprayCanSparkles}
+                icon2={faSpa}
+                icon3={faScissors}
+            />
+            <HeroSection />
+            <AnimatedDivider />
+            <MeetUsSection stylists={stylists} />
+            <TestimonialsSection />
+            <LocationSection />
+        </>
+    );
+}
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,5 @@
-
-import { HeroSection } from "@/components/(Home)/hero/HeroSection";
-import { MeetUsSection } from "@/components/(Home)/meetUs/MeetUsSection";
-import { AnimatedDivider } from "@/components/layout/AnimatedDivder";
-import { TestimonialsSection } from "@/components/(Home)/testimonials/TestimonialsSection";
-import { LocationSection } from "@/components/(Home)/location/LocationSection";
-import { BackgroundIcons } from "@/components/layout/BackgroundIcons";
-import { faScissors, faSpa, faSprayCanSparkles } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
 import { GetServerSideProps } from "next";
+import HomeClient from "./homeClient";
 
 interface stylistsProps {
     _id: string;
@@ -39,21 +31,10 @@ export const getServerSideProps: GetServerSideProps = async () => {
 };
 
 export default function Home({ initialStylists }: { initialStylists: stylistsProps[] }) {
-    const [stylists, setStylists] = useState<stylistsProps[]>(initialStylists);
 
     return (
         <>
-            <BackgroundIcons
-                rows={8}
-                icon1={faSprayCanSparkles}
-                icon2={faSpa}
-                icon3={faScissors}
-            />
-            <HeroSection />
-            <AnimatedDivider />
-            <MeetUsSection stylists={stylists} />
-            <TestimonialsSection />
-            <LocationSection />
+            <HomeClient initialStylists={initialStylists} />
         </>
     );
 }
